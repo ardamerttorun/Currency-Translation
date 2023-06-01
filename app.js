8bab597af4056e726f6c2c66bdba5c329a4039bd
Arda Torun <ardamerttorun@gmail.com>
2023-06-02 00:07:35 +0300

    Döviz kurlarının hesaplanması

--- app.js
+++ app.js
@@ -1,26 +1,43 @@
-const api_key="4073ea29ab98146eb56ad02a";
-const url="https://v6.exchangerate-api.com/v6/"+api_key;
+const api_key = "4073ea29ab98146eb56ad02a";
+const url = "https://v6.exchangerate-api.com/v6/" + api_key;
 
 //elemets
-const currency_one=document.querySelector("#currency-one");
-const currency_two=document.querySelector("#currency-two");
-const list_one=document.getElementById("list_one");
-const list_two=document.getElementById("list_two");
-const amount=document.getElementById("amount");
-const calculate=document.getElementById("calculate");
-const result=document.getElementById("result");
-
-
+const currency_one = document.querySelector("#currency-one");
+const currency_two = document.querySelector("#currency-two");
+const list_one = document.getElementById("list_one");
+const list_two = document.getElementById("list_two");
+const amount = document.getElementById("amount");
+const calculate = document.getElementById("calculate");
+const result = document.getElementById("result");
 
 fetch(url + "/codes")
-    .then(res=>res.json())
-    .then(data=>{
-        const items=data.supported_codes;
-        let options;
-        for(let item of items){
-            options += `<option value="${item[0]}">${item[1]}</option>`;
-        }
-        list_one.innerHTML=options;
-        list_two.innerHTML=options;
+  .then((res) => res.json())
+  .then((data) => {
+    const items = data.supported_codes;
+    let options;
+    for (let item of items) {
+      options += `<option value="${item[0]}">${item[1]}</option>`;
+    }
+    list_one.innerHTML = options;
+    list_two.innerHTML = options;
+  });
 
-    })
+calculate.addEventListener("click", function () {
+  const doviz1 = currency_one.value;
+  const doviz2 = currency_two.value;
+  const miktar = amount.value;
+
+  fetch(url + "/latest/" + doviz1)
+    .then((res) => res.json())
+    .then((data) => {
+        const sonuc=(data.conversion_rates[doviz2]*miktar).toFixed(3);
+        result.innerHTML = `
+            <div class="card border-primary">
+                <div class="card-body text-center" style="font-size: 30px;">
+                    ${miktar} ${doviz1}=${sonuc} ${doviz2}
+                </div>
+            </div>
+        `;
+      
+    });
+});
